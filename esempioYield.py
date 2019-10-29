6959913bccb42a888a769636fb0c5cac692f5da6
Biagio Boi <biagioboi@gmail.com>
2019-10-29 11:58:17 +0100

    Esercizi del 29 Ottobre

--- esempioYield.py
+++ esempioYield.py
@@ -1,12 +1,21 @@
+# Quando lo yield viene messo ad un assegnamento ex. x = yield allora aspetterà una send per ricevere
+# il valore da assegnare a x, contestualmente potrebbe anche restituire un valore 'yield x',
+# che restituisce il valore di x
+
 def raddoppia():
     while True:
-        x, y = yield
-        print("stampa tra un yield e l'altro del corpo del while. x = ", y)
+        # viene restiuito none in quanto non è specificato nulla dopo lo yield
+        # allo stesso tempo si aspetta una send() per dare un valore a x
+        yield
+        # print("stampa tra un yield e l'altro del corpo del while. x = ", x)
+        x = yield
+        # se al precedente yield non è stato assegnato nulla a x, la seguente operazione
+        # non è fattibile perchè None * int non si puo' fare
         x = yield x*2
         print("x = ", x)
 
 g = raddoppia()
 r = next(g)
-r = g.send((10, 20))
+next(g)
+r = g.send(10)
 print (r)
-r = next(g)
